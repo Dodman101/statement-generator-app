3fbdb262bbb823bf6951bde1c879250deb4382fb
Dodman101 <dodmanmwaura@gmail.com>
2024-11-22 09:15:05 +0300

    Added some comments that will assist with testing the api.

---
+++ tests/__init__.py
@@ -0,0 +1,23 @@
+"""Test suite for the application."""
+
+# Run the Tests
+# From the project root, run:
+
+# python -m unittest discover tests
+
+# pytest
+
+# pytest --cov=app tests/
+
+# View Detailed Reports:
+
+# On the terminal.
+
+# View missing lines:
+# pytest --cov=app --cov-report=term-missing
+
+# Generate an HTML report for a visual breakdown:
+# pytest --cov=app --cov-report=html
+
+# Open the report in a browser:
+# start htmlcov/index.html  # For Windows
