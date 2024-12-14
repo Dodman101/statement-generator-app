4bcc2356613e48b99e674ec751490bb76150139c
Dodman101 <dodmanmwaura@gmail.com>
2024-12-14 09:32:06 +0300

    Included the port to listen to

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ app = create_app()
 def page_not_found(e):
     return render_template('404.html'), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
